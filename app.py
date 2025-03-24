3d16af828855e78cd9a0ec96f0aef670d1fa5d69
SaraLaouini1 <laouinisara4@gmail.com>
2025-03-24 20:15:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ CORS(app, resources={
             "http://localhost:5173"
         ],
         "methods": ["GET", "POST", "OPTIONS"],
-        "allow_headers": ["Content-Type"],
+        "allow_headers": ["Content-Type", "Authorization"],
         "supports_credentials": True
     }
 })
