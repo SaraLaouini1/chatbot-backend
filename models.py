31429a2ec0cc4ca95bac92e0752c0ec378a14886
SaraLaouini1 <laouinisara4@gmail.com>
2025-04-03 20:20:45 +0000

    Update models.py

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ import bcrypt
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users' 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
